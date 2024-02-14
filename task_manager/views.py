2019e75f595f04e43f6118abe35b282be5add62a
Игорь Рудич <rudi4.clarinet@mail.ru>
2024-02-14 23:15:37 +0300

    try deploy on render

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from task_manager.settings import SECRET_KEY
 
 
 def index(request):
-    return render(request, 'index.html', context={'message': SECRET_KEY})
+    return render(request, 'index.html', context={'message': 'hello'})
