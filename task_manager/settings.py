9988d0498fc93975fd8d66ca14c0d8a35d31a329
Игорь Рудич <rudi4.clarinet@mail.ru>
2024-02-14 23:28:14 +0300

    try deploy on render 3

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['djangoapp-h53n.onrender.com']
 
 
 # Application definition
