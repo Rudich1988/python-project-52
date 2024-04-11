684c5a7079936eb1fd8b9c89987b0b44141c2175
Игорь Рудич <rudi4.clarinet@mail.ru>
2024-04-11 22:09:52 +0300

    add / in task_delete url

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,7 +10,7 @@ app_name = 'tasks'
 urlpatterns = [
     path('', TasksShowView.as_view(), name='tasks_show'),
     path('create/', TaskCreateView.as_view(), name='task_create'),
-    path('<int:pk>/delete', TaskDeleteView.as_view(), name='task_delete'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
     path('<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
     path('<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
 ]
