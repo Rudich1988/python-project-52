c47135896b321809740dffcc9209047f12ee97d1
Игорь Рудич <rudi4.clarinet@mail.ru>
2024-04-11 13:38:22 +0300

    add webserver to allowed_hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djangoapp-h53n.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['djangoapp-h53n.onrender.com', '127.0.0.1', 'webserver']
 
 
 # Application definition
